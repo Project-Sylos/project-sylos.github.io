d8109679b774b527f227e63cae67b525406f4236
Logan Maupin <logan@sylos.io>
2025-12-05 15:14:59 -0500

    Adding some basic fluff

---
+++ config.js
@@ -0,0 +1,10 @@
+// Configuration for Sylos landing page
+const CONFIG = {
+    links: {
+        github: "https://github.com/Project-Sylos",
+        discord: "https://discord.gg/sfYCzZQNQp",
+        documentation: "#docs",
+        downloads: "#downloads"
+    }
+};
+
