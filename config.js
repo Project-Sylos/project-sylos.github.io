fa72a3815cb7cbd145c7fd8e260d0a5875ed3c88
Sylos-Creator <logan@sylos.io>
2026-03-04 22:52:11 -0500

    Adjusting the links to contribute

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Configuration for Sylos landing page
 const CONFIG = {
     links: {
-        github: "https://github.com/Project-Sylos",
+        codeberg: "https://codeberg.org/Sylos",
         discord: "https://discord.gg/sfYCzZQNQp",
         documentation: "#docs",
         downloads: "#downloads"
